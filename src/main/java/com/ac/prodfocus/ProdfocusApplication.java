d09b5033ef94e3cfc033b2b4602bb69be5be44e9
AlissonSouto7 <alissonprogp1@gmail.com>
2024-05-20 10:09:30 -0300

    Configuracao da API, Banco, YML e pasta das entidades

---
+++ src/main/java/com/ac/prodfocus/ProdfocusApplication.java
@@ -0,0 +1,12 @@
+package com.ac.prodfocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdfocusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdfocusApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ac/prodfocus/ProdfocusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ac.prodfocus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdfocusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
